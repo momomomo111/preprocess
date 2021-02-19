33806f45b037a8ef63a5a7fd1a827e85245fdffa
momomomo111 <mossann0510@outlook.jp>
2021-02-19 09:20:53 +0900

    add crop, fix flip

--- flip.py
+++ flip.py
@@ -6,16 +6,15 @@ for name in glob.glob('./orig/*'):
     img = cv2.imread(name)
 
     print(img.shape)
-    # (225, 400, 3)
 
+    # 上下反転（upside down）
     img_flip_ud = cv2.flip(img, 0)
     cv2.imwrite('./add/' + name[7:-4] + '_ud.jpg', img_flip_ud)
-    # True
 
+    # 左右反転（left and right reversals）
     img_flip_lr = cv2.flip(img, 1)
     cv2.imwrite('./add/' + name[7:-4] + '_lr.jpg', img_flip_lr)
-    # True
 
+    # 上下左右反転（upside down, down side up）
     img_flip_ud_lr = cv2.flip(img, -1)
     cv2.imwrite('./add/' + name[7:-4] + '_ud_lr.jpg', img_flip_ud_lr)
-    # True
